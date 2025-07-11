9abd29813e04add10bced343ed9f84886217855a
adham <=adhambekboylanov@gmail.com>
2025-07-11 18:46:15 +0500

    saqlandi2

---
+++ configapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+def json_qaytar(request):
+    return JsonResponse({"vrebvefrgbth":"vtrwecgthb","vtebrvtr":"tv4tv4"})
---
+++ configapp1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+def json_qaytar1(request):
+    return JsonResponse({"evwbgrtnhjm":"fgtrhytu","ebrtnbf":"cfcevgrbrve"})
+
